8d08d286060e06f18014c3bbdd79d6fff0ba56be
mozakir178 <mozakir178@gmail.com>
2025-12-04 01:07:55 +0530

    class code

---
+++ create-elements/script.js
@@ -0,0 +1,59 @@
+// ------------------------------
+// TASK 1: SHOW NOTIFICATION
+// - Create a <p> element with class "widget"
+// - textContent: "This is a notification!"
+// - Append to #notificationContainer
+// ------------------------------
+
+
+
+
+
+// ------------------------------
+// TASK 2: ADD USER PROFILE
+// - Inputs: #userName, #userAge, #userCourse
+// - Create div.profile-card
+// - Create <p> for Name, Age, Course
+// - Append <p> to profile-card
+// - Append profile-card to #profileContainer
+// ------------------------------
+
+
+
+
+
+
+// ------------------------------
+// TASK 3: SHOW PRODUCTS FROM ARRAY
+// - Array provided: products
+// - For each product: create div.product-card.widget
+// - Create <p> for name and price
+// - Append <p> to card, append card to #productContainer
+// ------------------------------
+const products = [
+  { name: "Product 1", price: "$10" },
+  { name: "Product 2", price: "$20" },
+  { name: "Product 3", price: "$15" }
+];
+
+
+
+
+
+
+
+// ------------------------------
+// TASK 4: ADD COLOR BOX
+// - Create div.widget
+// - width & height: 50px
+// - backgroundColor: random hex
+// - Append to #colorContainer
+// ------------------------------
+
+// ------------------------------
+// TASK 5: ADD MESSAGE
+// - Input: #messageInput
+// - Create div.widget, set textContent
+// - Append to #messageContainer
+// - Clear input
+// ------------------------------
